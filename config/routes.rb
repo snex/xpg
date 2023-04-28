5bc154e13f9c2056769a2fc23831727aee3f0784
David Havlicek <snex00@protonmail.com>
2023-04-28 14:45:04 -0700

    map default route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "wallets#index"
 end
