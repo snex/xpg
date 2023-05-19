7bb0ea3fbfa86800e55276f77c769082464d9a9b
David Havlicek <snex00@protonmail.com>
2023-05-18 19:16:16 -0700

    update log_level for staging/production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,7 +37,10 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :error
+  config.log_level = :info
+  Sidekiq.configure_server do |config|
+    config.logger.level = 'INFO'
+  end
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
