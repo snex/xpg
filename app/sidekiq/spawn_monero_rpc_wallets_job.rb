0fc1d9b98dfd3c96dc8021bc55360b31b1ff8b7f
David Havlicek <snex00@protonmail.com>
2023-05-24 14:30:18 -0700

    ensure that Wallet#running? is able to be called via web node that may not be running the monero-wallet-rpc proc, though we lose some visibility in cases where the proc crashes and we have to wait for it to be picked up again. Fixes #46

--- app/sidekiq/spawn_monero_rpc_wallets_job.rb
+++ app/sidekiq/spawn_monero_rpc_wallets_job.rb
@@ -4,8 +4,9 @@ class SpawnMoneroRpcWalletsJob
   include Sidekiq::Job
 
   def perform
-    Wallet.pluck(:id).each do |wallet_id|
-      MoneroRpcWalletJob.perform_async(wallet_id)
+    Wallet.all.each do |wallet|
+      wallet.update_pid!
+      MoneroRpcWalletJob.perform_async(wallet.id)
     end
   end
 end
